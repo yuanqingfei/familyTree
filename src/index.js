16ac6de1f3cf7dbf0d97c580ebade293b552affb
Aaron.Yuan <yuanqingfei.126.com>
2018-02-11 13:35:00 -0500

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Composite from './components/composite.js';
-
-
-ReactDOM.render(<Composite />, document.getElementById('content'));
